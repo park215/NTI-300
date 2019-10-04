6d41b6805820b7479ad106b351046d1922be73e4
park215 <55862690+park215@users.noreply.github.com>
2019-10-03 19:53:23 -0700

    Update updating_webpage.sh

--- updating_webpage.sh
+++ updating_webpage.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-time=$(date)
-echo "<html><body><h1>hi there, it is $time, how are you?</h1></body></html>" > /var/www/html/index.html
+#!/bin/bash                 
+time=$(date)                #sets variable time
+echo "<html><body><h1>hi there, it is $time, how are you?</h1></body></html>" > /var/www/html/index.html  #create html page with time var
