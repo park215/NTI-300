09e6a6e77aeaea1fcf89c41c8ddd201edb3cbc6c
park215 <55862690+park215@users.noreply.github.com>
2019-10-10 19:42:28 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-if [ -e /usr/bin/wget ]; then exit 0; fi
+if [ -e /usr/bin/wget ]; then 
+  exit 0;
+fi
 yum -y install wget
 wget https://raw.githubusercontent.com/park215/NTI-300/master/packages.txt
 for packages in $(cat packages.txt); do 
