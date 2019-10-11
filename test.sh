c028f978bbf73b4cb721a630f5cd66d16514128e
root <root@enviroment.us-central1-a.c.loyal-karma-254202.internal>
2019-10-11 02:53:04 +0000

    added the test status tool

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+myvar=""
+if [ -z "$1" ] ; then
+ echo "you didnt provide an argument" 
+ exit 0;
+fi
+
+status=$(systemctl status $1 | grep Active | awk '{print $2}')
+inactive="inactive"
+
+if [ $status == $inactive ]; then 
+	echo "noooo its off"; 
+else
+	echo "My status is $status"
+fi
