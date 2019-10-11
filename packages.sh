5217d63d9a5fee0b6e5eb4e2ddb147fd8013828f
park215 <55862690+park215@users.noreply.github.com>
2019-10-10 18:34:28 -0700

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum -y install wget
+wget https://raw.githubusercontent.com/park215/NTI-300/master/packages.txt
 for packages in $(cat packages.txt); do 
   yum -y install $packages
 done
