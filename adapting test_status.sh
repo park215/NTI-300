61b56df2cbd5ba19309098f3167fa154ed5dca88
park215 <55862690+park215@users.noreply.github.com>
2019-10-18 16:10:30 -0700

    Update adapting test_status.sh

--- adapting test_status.sh
+++ adapting test_status.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
-
 read -p  "enter package or service you want to install:" myvar
-
 status2=$( yum list installed | grep "$myvar" | grep -v "tools" | awk -F "." '{print $1}' )
-status=$(systemctl status $3 | grep Active | awk '{print $4}')
-inactive="inactive"
-
 if [[ $status2 == $myvar ]]; then
         echo "this package or service is already installed"
-        exit 0;
-
-else
+elif
         read -p "do you want to install this program: " yn
         case $yn in
                 [Yy]* ) yum -y install "$myvar";;
                 [Nn]* ) exit 0;;
                 * ) echo "please answer yes or no";;
-                esac
-fi
-
-if [[ $status == $inactive ]]; then
-        read -p "the service is off would you like to turn it on?"; yn
+        esac
+status1=$(systemctl status "$myvar" | grep Active | awk '{print $2}')
+inactive="inactive"
+then [[ $status1 == $inactive ]];
+        read -p "the service is off would you like to turn it on?" yn
         case $yn in
-                [Yy]* ) systemctl start "$myvar"; break;;
+                [Yy]* ) systemctl start "$myvar";;
                 [Nn]* ) exit;;
                 * ) echo "please answer yes or no";;
         esac
-
 fi
