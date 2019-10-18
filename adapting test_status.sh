09c370236e70c585912e1e9d19606b629eed2d9f
park215 <55862690+park215@users.noreply.github.com>
2019-10-17 23:14:50 -0700

    Update adapting test_status.sh

--- adapting test_status.sh
+++ adapting test_status.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
 read -p  "enter package or service you want to install:" myvar
-
-status2=$( yum list installed | grep "$myvar" | grep -v "tools" | awk -F "." '{print $1}' )
-
-echo $myvar
-echo $status2
-if [ $status == $myvar ]; then
+status2=$( yum list installed | grep "$myvar" | grep -v "tools" | awk -F "." '{pri
+nt $1}' )
+if [[ $status2 == $myvar ]]; then
         echo "this package or service is already installed"
         exit 0;
-
 else
         read -p "do you want to install this program: " yn
         case $yn in
