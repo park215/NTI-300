1dd0b466078258399fb5b8f62403c122fb34e879
park215 <55862690+park215@users.noreply.github.com>
2019-10-19 09:12:26 -0700

    Update adapting extra credit.sh

--- adapting extra credit.sh
+++ adapting extra credit.sh
@@ -16,9 +16,33 @@ inactive="inactive"
         do
         read -p "the service is off would you like to turn it on? " yn
         case $yn in
-                [Yy]* ) service start "$@";;
+                [Yy]* ) systemctl start "$@";;
                 [Nn]* ) exit 0;;
                 * ) echo "please answer yes or no";;
         esac
+        exit 0;
 done
+
+fi
+
+read -p  "enter package or service you want to install:" myvar
+status2=$( yum list installed | grep "$myvar" | grep -v "tools" | awk -F "." '{print $1}' )
+if [[ $status2 == $myvar ]]; then
+        echo "this package or service is already installed"
+elif
+        read -p "do you want to install this program: " yn
+        case $yn in
+                [Yy]* ) yum -y install "$myvar";;
+                [Nn]* ) exit 0;;
+                * ) echo "please answer yes or no";;
+        esac
+status1=$(systemctl status "$myvar" | grep Active | awk '{print $2}')
+inactive="inactive"
+then [[ $status1 == $inactive ]];
+        read -p "the service is off would you like to turn it on? " yn
+        case $yn in
+                [Yy]* ) systemctl start "$myvar";;
+                [Nn]* ) exit 0;;
+                * ) echo "please answer yes or no";;
+        esac
 fi
