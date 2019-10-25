9f686c3fb01d97ea91414f4a286c86f6ecb7913f
park215 <55862690+park215@users.noreply.github.com>
2019-10-24 19:51:37 -0700

    Create django.sh

---
+++ django.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+yum -y install python-pip
+pip install virtualenv
+pip install --upgrade pip
+mkdir ~/newproject
+cd ~/newproject
+virtualenv newenv
+source newenv/bin/activate
+pip install django
+django-admin startproject newproject
+cd newproject
+python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@newproject.com', 'P@ssword1')" | python manage.py shell                #work around this line
+myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$myip\'\]/g" newproject/settings.py 
+python manage.ppy runserver 0.0.0.0:8000
