c6a04558911368d38c1fe840193c541e803d6996
park215 <55862690+park215@users.noreply.github.com>
2019-10-28 14:11:31 -0700

    Update adapting extra credit.sh

--- adapting extra credit.sh
+++ adapting extra credit.sh
@@ -3,22 +3,22 @@
 for i in $@
 do
 echo "installing: "$@
-status2=$( yum list installed | grep "$myvar" | grep -v "tools" |  awk -F "." '{print $1}' )
-if [[ $status2 == $myvar ]]; then
+status2=$( yum list installed | grep "$@" | grep -v "tools" |  awk -F "." '{print $1}' )
+if [[ $status2 == $@ ]]; then
         echo "this package or service is already installed"
 elif
         read -p "do you want to install this program: " yn
         case $yn in
-                [Yy]* ) yum -y install "$myvar";;
+                [Yy]* ) yum -y install "$@";;
                 [Nn]* ) exit 0;;
                 * ) echo "please answer yes or no";;
         esac
-status1=$(systemctl status "$myvar" | grep Active | awk '{print $2}')
+status1=$(systemctl status "$@" | grep Active | awk '{print $2}')
 inactive="inactive"
 then [[ $status1 == $inactive ]];
         read -p "the service is off would you like to turn it on? " yn
         case $yn in
-                [Yy]* ) systemctl start "$myvar";;
+                [Yy]* ) systemctl start "$@";;
                 [Nn]* ) exit 0;;
                 * ) echo "please answer yes or no";;
         esac
