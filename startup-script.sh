792d1019f31b398df99376a6752c42115ecae6dd
park215 <55862690+park215@users.noreply.github.com>
2019-11-07 19:18:35 -0800

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -8,9 +8,9 @@ import json
 credentials = GoogleCredentials.get_application_default()
 compute = discovery.build('compute', 'v1', credentials=credentials)
 
-project = 'NTI-300'
-zone = 'us-centtral1-a'
-name = 'test'
+project = 'loyal-karma-254202'
+zone = 'us-central1-a'
+name = 'test2'
 
 def list_instances(compute, project, zone):
 	result = compute.instances().list(project=project, zone=zone).execute()
@@ -22,64 +22,65 @@ def create_instance(compute, project, zone, name):
 		project='centos-cloud', family='centos-7').execute()
 		
 	source_disk_image = image_response['selfLink']
-	machine_type = "zone/%s/machineTypes/f1-micro" % zone
+	machine_type = "zones/%s/machineTypes/f1-micro" % zone
 	
-config = {
-	'name': name,
-	'machineType': machine_type,
+	config = {
+		'name': name,
+		'machineType': machine_type,
 		
-	#specify the boot disk and the image as a source_disk_image
-	'disk': [
-		{
+		#specify the boot disk and the image as a source_disk_image
+		'disk': [
+	 	   {
 			'boot': True,
 			'autoDelete': True,
 			'initializeParams': {
-				'sourceImage': source_disk_image,
-			}
-		}
-	],
+			   'sourceImage': source_disk_image,
+			 }
+		   }
+		],
 		
-	#specify network interface with NAT to access the public
-	#internet
-	'networkInterfaces': [{
-		'network': 'global/networks/default',
-		'accessConfigs': [
-			{'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+		#specify network interface with NAT to access the public
+		#internet
+		'networkInterfaces': [{
+		   'network': 'global/networks/default',
+		   'accessConfigs': [
+		      {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+		   ]
+		}],
+		
+		'serviceAccounts': [{
+		   'email': 'default',
+		   'scopes': [
+		       'https://www.googleapis.com/auth/devstorage.read_write',
+		       'https://www.googleapis.com/auth/logging.write'
+	           ]  
+		}],
+		
+		#Enable http/https for select instances
+		"labels": {
+		"http-server": "",
+		"https-server": ""
+		},
+		
+		"tags": {
+		"items": [
+		"http-server" 
+		"https-server"
 		]
-	}],
+		},
 		
-	'serviceAccounts': [{
-		'email': 'default',
-		'scopes': [
-			'https://www.googleapis.com/auth/devstorage.read_write',
-			'https://www.googleapis.com/auth/logging.write'
-		]
-	}],
-		
-	#Enable http/https for select instances
-	"labels": {
-	"http-server": "",
-	"https-server": ""
-	},
-		
-	"tags": {
-	"items": [
-	"http-server": "",
-	"https-server": ""
-	]
-	},
-		
-	#metadata is readable from the instances and allow
-	#you to pass config from deployment script to instances
-	'metadata': {
-		'items': [{
+		#metadata is readable from the instances and allow
+		#you to pass config from deployment script to instances
+		'metadata': {
+		   'items': [{
 			
-			#startup script is automattily exectued by the instance upon startup
-			'key': 'startup-script',
-			'value': startup_script
-		}]
+		   #startup script is automattily exectued by the instance upon startup
+		   'key': 'startup-script',
+		   'value': startup_script
+
+		 }]
+	   }
 	}
-}
 
 	return compute.instances().insert(
 		project=project,
