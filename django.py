e54f5c325509b9dcaf0c3490739b82afcdd2a20a
park215 <55862690+park215@users.noreply.github.com>
2019-12-05 18:42:52 -0800

    Update django.py

--- django.py
+++ django.py
@@ -12,7 +12,7 @@ os.system ('adduser -M django' + \
 def local_repo():
     repo="""[local-epel]
 name=NTI300 EPEL
-baseurl=http://35.223.7.191/epel/
+baseurl=http://34.69.19.143/epel/
 gpgcheck=0
 enabled=1"""
     os.system('for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done')
